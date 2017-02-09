2a7ff02cfd2a1aa3017f0fa01db88d4468b2423f
Aurelien Burget <aurelion@gmail.com>
2017-02-09 12:58:55 +0100

    Adding validation and associations in the models

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,8 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
-end
+  validates :description, presence: true
+  validates :ingredient, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient,
+    message: "should have once per ingredient" }, presence: true
+ end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cocktail.destroy_all
+Ingredient.destroy_all
+Dose.destroy_all
+
+c1 = Cocktail.new(name: "mojito")
+c2 = Cocktail.new(name: "caipirinha")
+
+i1 = Ingredient.new(name: "lemon")
+i2 = Ingredient.new(name: "orange")
+
+d1 = Dose.new(description: "lemon-mojito", cocktail: c1, ingredient: i1)
+d2 = Dose.new(description: "lemon-caipi", cocktail: c2, ingredient: i1)
+d3 = Dose.new(description: "lemon-caipi2", cocktail: c2, ingredient: i1)
+
+c1.save!
+c2.save!
+i1.save!
+i2.save!
+d1.save!
+d2.save!
+# d3.save! testing validation and association
