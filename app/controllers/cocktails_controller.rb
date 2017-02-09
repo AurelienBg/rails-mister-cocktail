ae39d7ab0700e0aa1ebf628e14cf5ec4e14c358e
Aurelien Burget <aurelion@gmail.com>
2017-02-09 17:23:38 +0100

    adding the missing actions for the controllers

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-  before_action :set_cocktail, only: [:show, :edit ]
+  before_action :set_cocktail, only: [:show ]
 
   def index
     @cocktails = Cocktail.all
@@ -7,9 +7,13 @@ class CocktailsController < ApplicationController
 
   def new
     @cocktail = Cocktail.new
+    @dose = Dose.new
+    @dose.cocktail_id = @cocktail
   end
 
   def show
+    @dose = Dose.new
+    @dose.cocktail = @cocktail
   end
 
   def create
--- db/seeds.rb
+++ db/seeds.rb
@@ -12,18 +12,27 @@ Dose.destroy_all
 
 c1 = Cocktail.new(name: "mojito")
 c2 = Cocktail.new(name: "caipirinha")
+c3 = Cocktail.new(name: "whisky-coca")
+c4 = Cocktail.new(name: "Cubalibre")
 
 i1 = Ingredient.new(name: "lemon")
 i2 = Ingredient.new(name: "orange")
+i3 = Ingredient.create(name: "lemon")
+i4 = Ingredient.create(name: "ice")
+i5 = Ingredient.create(name: "mint leaves")
 
 d1 = Dose.new(description: "lemon-mojito", cocktail: c1, ingredient: i1)
 d2 = Dose.new(description: "lemon-caipi", cocktail: c2, ingredient: i1)
-d3 = Dose.new(description: "lemon-caipi2", cocktail: c2, ingredient: i1)
+d3 = Dose.new(description: "cubacita", cocktail: c3, ingredient: i4)
 
 c1.save!
 c2.save!
+c3.save!
+c4.save!
 i1.save!
 i2.save!
+i4.save!
+i5.save!
 d1.save!
 d2.save!
 # d3.save! testing validation and association
