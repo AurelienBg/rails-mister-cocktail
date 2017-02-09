75315570cc1d737d08dde0bba5b58ff1ac39526d
Aurelien Burget <aurelion@gmail.com>
2017-02-09 14:58:37 +0100

    adding actions of Cocktails controller

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,4 +1,33 @@
 class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: [:show, :edit ]
+
   def index
+    @cocktails = Cocktail.all
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def show
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktails_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
   end
 end
